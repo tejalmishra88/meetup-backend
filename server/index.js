05843cf5859a7cc2bf37f5b7dbb66bc3386e1935
tejalmishra88 <tejalmishra88@gmail.com>
2018-04-29 19:47:21 +1000

    postman get and post working

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import dbconfig from './config/db';
+import middlewaresconfig from './config/middlewares';
+import {MeetupRoutes } from './module';
 const app = express();
 dbconfig();
+middlewaresconfig(app);
+app.use('/api', [MeetupRoutes]);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, err => {
---
+++ server/module/meetups/model.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema} from 'mongoose';
+
+
+const MeetupSchema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  });
+  
+  export default mongoose.model('Meetup', MeetupSchema); 
